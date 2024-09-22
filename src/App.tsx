8bc47eb9c04794eff91f0f447e7b0330f107bcfe
ethanmouri <mouri.e@northeastern.edu>
2024-09-21 23:57:32 -0400

    First commit for web dev

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First Commit For Web Dev 
         </a>
       </header>
     </div>
